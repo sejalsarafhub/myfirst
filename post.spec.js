c1598572f0ee7764c0ded5049876a12e3beffe2a
Sejal Saraf <sejalsaraf0312@gmail.com>
2025-08-12 21:11:05 +0530

    Update post.spec.js

--- post.spec.js
+++ post.spec.js
@@ -1,10 +1,20 @@
-const { test, expect } = require("@playwright/test");
+import { test, expect } from "@playwright/test";
 
-test("GET with query params", async ({ request }) => {
-  const response = await request.get("https://www.saucedemo.com/v1/main.js"
-    );
+test("POST API - Login", async ({ request }) => {
+  const response = await request.post("https://api.restful-api.dev/objects", {
+    data: {
+   "name": "Apple MacBook Pro 16",
+   "data": {
+      "year": 2019,
+      "price": 1849.99,
+      "CPU model": "Intel Core i9",
+      "Hard disk size": "1 TB"
+   }
+}
+  });
 
   expect(response.status()).toBe(200);
-  
-  console.log(response.status());
+  console.log("Response:", response.status());
+
+
 });
